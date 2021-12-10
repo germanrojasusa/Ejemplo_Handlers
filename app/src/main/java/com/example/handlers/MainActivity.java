9360e7b89241fab5a1f33b1b9fc803a0bfd5b03a
german.rojas <german.rojas.mt@correo.usa.edu.co>
2021-12-09 22:04:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/handlers/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.handlers;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Matrix;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ProgressBar progressBar;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        progressBar.setProgress(0);
+        h1.start();
+    }
+
+    Handler c1 = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            int conteo = msg.getData().getInt("conteo");
+            String name = msg.getData().getString("nombre");
+
+            progressBar.setProgress(conteo+1);
+            textView.setText(String.valueOf(progressBar.getProgress()) +"%");
+
+            imageView.setScaleType(ImageView.ScaleType.MATRIX);   //required
+            imageView.setRotation(progressBar.getProgress()*40);
+            imageView.setImageResource(R.drawable.homer);
+
+        }
+    };
+    /*
+    Handler c2 = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            Matrix matrix = new Matrix();
+            imageView2.setScaleType(ImageView.ScaleType.MATRIX);   //required
+            matrix.postRotate((float) progressBar.getProgress()*40, random.nextInt(10), random.nextInt(10));
+            imageView2.setImageMatrix(matrix);
+
+
+        }
+    };
+
+     */
+
+    Thread h1 = new Thread(){
+        @Override
+        public void run() {
+            try {
+                Bundle dato = new Bundle();
+                Message message = c1.obtainMessage();
+
+                for (int i = 0; i<100; i++){
+                    Thread.sleep(200);
+                    dato.putInt("conteo",i);
+                    dato.putString("nombre", "Un dato tipo String");
+                    message.setData(dato);
+                    c1.sendMessage(message);
+                }
+            } catch (Throwable throwable)  {
+                throwable.printStackTrace();
+            } finally {
+                //finish();
+            }
+        }
+    };
+/*
+    Thread h2 = new Thread(){
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i<100; i++){
+                    Thread.sleep(200);
+                    c1.sendMessage(c1.obtainMessage(i));
+                }
+            } catch (Throwable throwable)  {
+                throwable.printStackTrace();
+            } finally {
+                //finish();
+            }
+        }
+    };
+
+ */
+
+}
